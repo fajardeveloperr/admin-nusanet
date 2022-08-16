ee46bb481c161eb60a0989a66a419d9e89fa3136
fajardeveloperr <fajar@nusa.net.id>
2022-08-16 15:29:27 +0700

    update-seed

--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -16,8 +16,126 @@ class UsersTableSeeder extends Seeder
     public function run()
     {
         User::create([
-            'name'      =>'Samuel',
+            'name'      =>'Fajar',
+            'email'     => 'fajar@nusa.net.id',
+            'password'  => bcrypt('12345678'),
+            'utype'     => 'AuthMaster',
+        ]);
+
+        User::create([
+            'name'      =>'Cut Amalia',
+            'email'     => 'cutamalia@nusa.net.id',
+            'password'  => bcrypt('12345678'),
+            'utype'     => 'AuthMaster',
+
+        ]);
+
+        User::create([
+
+            'name'      =>'samuel',
             'email'     => 'samuel@nusa.net.id',
+            'password'  => bcrypt('12345678'),
+            'utype'     => 'AuthMaster',
+        ]);
+
+        User::create([
+            'name'      =>'Riris',
+            'email'     => 'riris@nusa.net.id',
+            'password'  => bcrypt('12345678'),
+            'utype'     => 'AuthMaster',
+        ]);
+
+        User::create([
+            'name'      =>'Syafii',
+            'email'     => 'syafii@nusa.net.id',
+            'password'  => bcrypt('12345678'),
+            'utype'     => 'AuthMaster',
+
+        ]);
+
+        User::create([
+            'name'      =>'Jefri',
+            'email'     => 'jefri@nusa.net.id',
+            'password'  => bcrypt('12345678'),
+            'utype'     => 'AuthMaster',
+
+        ]);
+
+        User::create([
+            'name'      =>'Ali Putera',
+            'email'     => 'aliputera@nusa.net.id',
+            'password'  => bcrypt('12345678'),
+            'utype'     => 'AuthMaster',
+
+        ]);
+
+        User::create([
+            'name'      =>'Timmie',
+            'email'     => 'timmie@nusa.net.id',
+            'password'  => bcrypt('12345678'),
+            'utype'     => 'AuthMaster',
+
+        ]);
+
+        User::create([
+            'name'      =>'Sovian',
+            'email'     => 'sovian@nusa.net.id',
+            'password'  => bcrypt('12345678'),
+            'utype'     => 'AuthMaster',
+
+
+        ]);
+
+        User::create([
+            'name'      =>'Ramon',
+            'email'     => 'ramon@nusa.net.id',
+            'password'  => bcrypt('12345678'),
+            'utype'     => 'AuthMaster',
+
+        ]);
+
+        User::create([
+            'name'      =>'Bambang',
+            'email'     => 'bambang@nusa.net.id',
+            'password'  => bcrypt('12345678'),
+            'utype'     => 'AuthMaster',
+
+        ]);
+
+        User::create([
+            'name'      =>'Rudi',
+            'email'     => 'sahrudi@nusa.net.id',
+            'password'  => bcrypt('12345678'),
+            'utype'     => 'AuthMaster',
+
+        ]);
+
+        User::create([
+            'name'      =>'Budiman',
+            'email'     => 'budiman@nusa.net.id',
+            'password'  => bcrypt('12345678'),
+            'utype'     => 'AuthMaster',
+        ]);
+
+        User::create([
+            'name'      =>'Fani',
+            'email'     => 'fani@nusa.net.id',
+            'password'  => bcrypt('12345678'),
+            'utype'     => 'AuthMaster',
+
+        ]);
+
+        User::create([
+            'name'      =>'Jimmy',
+            'email'     => 'jimmy@nusa.net.id',
+            'password'  => bcrypt('12345678'),
+            'utype'     => 'AuthMaster',
+
+        ]);
+
+        User::create([
+            'name'      =>'Abdul Majid',
+            'email'     => 'abdulmajid@nusa.net.id',
             'password'  => bcrypt('12345678'),
             'utype'     => 'AuthMaster',
 
