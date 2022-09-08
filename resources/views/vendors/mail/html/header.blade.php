052e7c9dfb37b2dd8adebc5ce29137171ebf10f8
fajardeveloperre <fajar@nusa.net.id>
2022-09-08 10:08:24 +0700

    updated-regards

--- resources/views/vendors/mail/html/header.blade.php
+++ resources/views/vendors/mail/html/header.blade.php
@@ -2,7 +2,7 @@
     <td class="header">
         <a href="{{ $url }}" style="display: inline-block;">
             @if (trim($slot) === 'Laravel')
-                <img src="https://rega.nusa.net.id/vendors/images/logo-nusanet.png" class="logo" alt="fajar">
+                <img src="https://rega.nusa.net.id/vendors/images/logo-nusanet.png" class="logo" alt="Nusanet">
             @else
                 {{ $slot }}
             @endif
