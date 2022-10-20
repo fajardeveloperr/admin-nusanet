472bc7df5c27ea4daa7cdb5c5db8c8fc5174648f
SamuelNusanetID <samuel@nusa.net.id>
2022-10-20 10:32:45 +0700

    Fix: Perbaikan User Seeder

--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -16,217 +16,222 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        User::insert([
-            [
-                'employee_id' => '0201826',
-                'name'      => 'Cut Amalia',
-                'email'     => 'cutamalia@nusa.net.id',
-                'password'  => bcrypt('Nusanet2022!'),
-                'utype'     => 'AuthMaster',
-                'isApprovedByAdmin'     => 1,
-                'email_verified_at' => Carbon::now(),
-                'created_at' => Carbon::now(),
-                'updated_at' => Carbon::now()
-            ],
-            [
-                'employee_id' => '0202233',
-                'name'      => 'M. Fikri Pasaribu',
-                'email'     => 'fikri@nusa.net.id',
-                'password'  => bcrypt('Nusanet2022!'),
-                'utype'     => 'AuthSales',
-                'isApprovedByAdmin'     => 1,
-                'email_verified_at' => Carbon::now(),
-                'created_at' => Carbon::now(),
-                'updated_at' => Carbon::now()
-            ],
+        // User::insert([
+        //     [
+        //         'employee_id' => '0201826',
+        //         'name'      => 'Cut Amalia',
+        //         'email'     => 'cutamalia@nusa.net.id',
+        //         'password'  => bcrypt('Nusanet2022!'),
+        //         'utype'     => 'AuthMaster',
+        //         'isApprovedByAdmin'     => 1,
+        //         'email_verified_at' => Carbon::now(),
+        //         'created_at' => Carbon::now(),
+        //         'updated_at' => Carbon::now()
+        //     ],
+        //     [
+        //         'employee_id' => '0202233',
+        //         'name'      => 'M. Fikri Pasaribu',
+        //         'email'     => 'fikri@nusa.net.id',
+        //         'password'  => bcrypt('Nusanet2022!'),
+        //         'utype'     => 'AuthSales',
+        //         'isApprovedByAdmin'     => 1,
+        //         'email_verified_at' => Carbon::now(),
+        //         'created_at' => Carbon::now(),
+        //         'updated_at' => Carbon::now()
+        //     ],
+        // ]);
+
+        User::create([
+            'employee_id' => '0202241',
+            'name'      => 'Fajar',
+            'email'     => 'fajar@nusa.net.id',
+            'password'  => bcrypt('Nusanet2022!'),
+            'utype'     => 'AuthMaster',
+            'isApprovedByAdmin'     => 1,
+            'email_verified_at' => Carbon::now(),
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now()
         ]);
 
-        // User::create([
-        //     'employee_id' => '',
-        //     'name'      => 'Fajar',
-        //     'email'     => 'fajar@nusa.net.id',
-        //     'password'  => bcrypt('12345678'),
-        //     'utype'     => 'AuthMaster',
-        //     'email_verified_at' => Carbon::now(),
-        //     'created_at' => Carbon::now(),
-        //     'updated_at' => Carbon::now()
-        // ]);
+        User::create([
+            'employee_id' => '0201826',
+            'name'      => 'Cut Amalia',
+            'email'     => 'cutamalia@nusa.net.id',
+            'password'  => bcrypt('Nusanet2022!'),
+            'utype'     => 'AuthMaster',
+            'isApprovedByAdmin'     => 1,
+            'email_verified_at' => Carbon::now(),
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now()
 
-        // User::create([
-        //     'employee_id' => '',
-        //     'name'      => 'Cut Amalia',
-        //     'email'     => 'cutamalia@nusa.net.id',
-        //     'password'  => bcrypt('12345678'),
-        //     'utype'     => 'AuthMaster',
-        //     'email_verified_at' => Carbon::now(),
-        //     'created_at' => Carbon::now(),
-        //     'updated_at' => Carbon::now()
+        ]);
 
-        // ]);
+        User::create([
+            'employee_id' => '0202240',
+            'name'      => 'Samuel',
+            'email'     => 'samuel@nusa.net.id',
+            'password'  => bcrypt('Nusanet2022!'),
+            'utype'     => 'AuthMaster',
+            'isApprovedByAdmin'     => 1,
+            'email_verified_at' => Carbon::now(),
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now()
+        ]);
 
-        // User::create([
-        //     'employee_id' => '',
-        //     'name'      => 'samuel',
-        //     'email'     => 'samuel@nusa.net.id',
-        //     'password'  => bcrypt('12345678'),
-        //     'utype'     => 'AuthMaster',
-        //     'email_verified_at' => Carbon::now(),
-        //     'created_at' => Carbon::now(),
-        //     'updated_at' => Carbon::now()
-        // ]);
+        User::create([
+            'employee_id' => '0202206',
+            'name'      => 'Riris',
+            'email'     => 'riris@nusa.net.id',
+            'password'  => bcrypt('Nusanet2022!'),
+            'utype'     => 'AuthMaster',
+            'isApprovedByAdmin'     => 1,
+            'email_verified_at' => Carbon::now(),
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now()
+        ]);
 
-        // User::create([
-        //     'employee_id' => '',
-        //     'name'      => 'Riris',
-        //     'email'     => 'riris@nusa.net.id',
-        //     'password'  => bcrypt('12345678'),
-        //     'utype'     => 'AuthMaster',
-        //     'email_verified_at' => Carbon::now(),
-        //     'created_at' => Carbon::now(),
-        //     'updated_at' => Carbon::now()
-        // ]);
+        User::create([
+            'employee_id' => '0200519',
+            'name'      => 'Syafii',
+            'email'     => 'syafii@nusa.net.id',
+            'password'  => bcrypt('Nusanet2022!'),
+            'utype'     => 'AuthMaster',
+            'isApprovedByAdmin'     => 1,
+            'email_verified_at' => Carbon::now(),
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now()
+        ]);
 
-        // User::create([
-        //     'employee_id' => '',
-        //     'name'      => 'Syafii',
-        //     'email'     => 'syafii@nusa.net.id',
-        //     'password'  => bcrypt('12345678'),
-        //     'utype'     => 'AuthMaster',
-        //     'email_verified_at' => Carbon::now(),
-        //     'created_at' => Carbon::now(),
-        //     'updated_at' => Carbon::now()
+        User::create([
+            'employee_id' => '0201203',
+            'name'      => 'Jefri',
+            'email'     => 'jefri@nusa.net.id',
+            'password'  => bcrypt('Nusanet2022!'),
+            'utype'     => 'AuthMaster',
+            'isApprovedByAdmin'     => 1,
+            'email_verified_at' => Carbon::now(),
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now()
+        ]);
 
-        // ]);
+        User::create([
+            'employee_id' => '0200622',
+            'name'      => 'Ali Putera',
+            'email'     => 'aliputera@nusa.net.id',
+            'password'  => bcrypt('Nusanet2022!'),
+            'utype'     => 'AuthMaster',
+            'isApprovedByAdmin'     => 1,
+            'email_verified_at' => Carbon::now(),
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now()
+        ]);
 
-        // User::create([
-        //     'employee_id' => '',
-        //     'name'      => 'Jefri',
-        //     'email'     => 'jefri@nusa.net.id',
-        //     'password'  => bcrypt('12345678'),
-        //     'utype'     => 'AuthMaster',
-        //     'email_verified_at' => Carbon::now(),
-        //     'created_at' => Carbon::now(),
-        //     'updated_at' => Carbon::now()
+        User::create([
+            'employee_id' => '0201408',
+            'name'      => 'Timmie',
+            'email'     => 'timmie@nusa.net.id',
+            'password'  => bcrypt('Nusanet2022!'),
+            'utype'     => 'AuthMaster',
+            'isApprovedByAdmin'     => 1,
+            'email_verified_at' => Carbon::now(),
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now()
+        ]);
 
-        // ]);
+        User::create([
+            'employee_id' => '0200925',
+            'name'      => 'Sovian',
+            'email'     => 'sovian@nusa.net.id',
+            'password'  => bcrypt('Nusanet2022!'),
+            'utype'     => 'AuthMaster',
+            'isApprovedByAdmin'     => 1,
+            'email_verified_at' => Carbon::now(),
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now()
+        ]);
 
-        // User::create([
-        //     'employee_id' => '',
-        //     'name'      => 'Ali Putera',
-        //     'email'     => 'aliputera@nusa.net.id',
-        //     'password'  => bcrypt('12345678'),
-        //     'utype'     => 'AuthMaster',
-        //     'email_verified_at' => Carbon::now(),
-        //     'created_at' => Carbon::now(),
-        //     'updated_at' => Carbon::now()
+        User::create([
+            'employee_id' => '0201908',
+            'name'      => 'Ramon',
+            'email'     => 'ramon@nusa.net.id',
+            'password'  => bcrypt('Nusanet2022!'),
+            'utype'     => 'AuthSales',
+            'isApprovedByAdmin'     => 1,
+            'email_verified_at' => Carbon::now(),
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now()
+        ]);
 
-        // ]);
+        User::create([
+            'employee_id' => '0200907',
+            'name'      => 'Bambang',
+            'email'     => 'bambang@nusa.net.id',
+            'password'  => bcrypt('Nusanet2022!'),
+            'utype'     => 'AuthSales',
+            'isApprovedByAdmin'     => 1,
+            'email_verified_at' => Carbon::now(),
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now()
+        ]);
 
-        // User::create([
-        //     'employee_id' => '',
-        //     'name'      => 'Timmie',
-        //     'email'     => 'timmie@nusa.net.id',
-        //     'password'  => bcrypt('12345678'),
-        //     'utype'     => 'AuthMaster',
-        //     'email_verified_at' => Carbon::now(),
-        //     'created_at' => Carbon::now(),
-        //     'updated_at' => Carbon::now()
+        User::create([
+            'employee_id' => '0201112',
+            'name'      => 'Rudi',
+            'email'     => 'sahrudi@nusa.net.id',
+            'password'  => bcrypt('Nusanet2022!'),
+            'utype'     => 'AuthSales',
+            'isApprovedByAdmin'     => 1,
+            'email_verified_at' => Carbon::now(),
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now()
+        ]);
 
-        // ]);
+        User::create([
+            'employee_id' => '0201005',
+            'name'      => 'Budiman',
+            'email'     => 'budiman@nusa.net.id',
+            'password'  => bcrypt('Nusanet2022!'),
+            'utype'     => 'AuthSales',
+            'isApprovedByAdmin'     => 1,
+            'email_verified_at' => Carbon::now(),
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now()
+        ]);
 
-        // User::create([
-        //     'employee_id' => '',
-        //     'name'      => 'Sovian',
-        //     'email'     => 'sovian@nusa.net.id',
-        //     'password'  => bcrypt('12345678'),
-        //     'utype'     => 'AuthMaster',
-        //     'email_verified_at' => Carbon::now(),
-        //     'created_at' => Carbon::now(),
-        //     'updated_at' => Carbon::now()
+        User::create([
+            'employee_id' => '0201516',
+            'name'      => 'Fani',
+            'email'     => 'fani@nusa.net.id',
+            'password'  => bcrypt('Nusanet2022!'),
+            'utype'     => 'AuthSales',
+            'isApprovedByAdmin'     => 1,
+            'email_verified_at' => Carbon::now(),
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now()
+        ]);
 
+        User::create([
+            'employee_id' => '0201318',
+            'name'      => 'Jimmy',
+            'email'     => 'jimmy@nusa.net.id',
+            'password'  => bcrypt('Nusanet2022!'),
+            'utype'     => 'AuthSales',
+            'isApprovedByAdmin'     => 1,
+            'email_verified_at' => Carbon::now(),
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now()
+        ]);
 
-        // ]);
-
-        // User::create([
-        //     'employee_id' => '',
-        //     'name'      => 'Ramon',
-        //     'email'     => 'ramon@nusa.net.id',
-        //     'password'  => bcrypt('12345678'),
-        //     'utype'     => 'AuthMaster',
-        //     'email_verified_at' => Carbon::now(),
-        //     'created_at' => Carbon::now(),
-        //     'updated_at' => Carbon::now()
-
-        // ]);
-
-        // User::create([
-        //     'employee_id' => '',
-        //     'name'      => 'Bambang',
-        //     'email'     => 'bambang@nusa.net.id',
-        //     'password'  => bcrypt('12345678'),
-        //     'utype'     => 'AuthMaster',
-        //     'email_verified_at' => Carbon::now(),
-        //     'created_at' => Carbon::now(),
-        //     'updated_at' => Carbon::now()
-
-        // ]);
-
-        // User::create([
-        //     'employee_id' => '',
-        //     'name'      => 'Rudi',
-        //     'email'     => 'sahrudi@nusa.net.id',
-        //     'password'  => bcrypt('12345678'),
-        //     'utype'     => 'AuthMaster',
-        //     'email_verified_at' => Carbon::now(),
-        //     'created_at' => Carbon::now(),
-        //     'updated_at' => Carbon::now()
-
-        // ]);
-
-        // User::create([
-        //     'employee_id' => '',
-        //     'name'      => 'Budiman',
-        //     'email'     => 'budiman@nusa.net.id',
-        //     'password'  => bcrypt('12345678'),
-        //     'utype'     => 'AuthMaster',
-        //     'email_verified_at' => Carbon::now(),
-        //     'created_at' => Carbon::now(),
-        //     'updated_at' => Carbon::now()
-        // ]);
-
-        // User::create([
-        //     'employee_id' => '',
-        //     'name'      => 'Fani',
-        //     'email'     => 'fani@nusa.net.id',
-        //     'password'  => bcrypt('12345678'),
-        //     'utype'     => 'AuthMaster',
-        //     'email_verified_at' => Carbon::now(),
-        //     'created_at' => Carbon::now(),
-        //     'updated_at' => Carbon::now()
-
-        // ]);
-
-        // User::create([
-        //     'employee_id' => '',
-        //     'name'      => 'Jimmy',
-        //     'email'     => 'jimmy@nusa.net.id',
-        //     'password'  => bcrypt('12345678'),
-        //     'utype'     => 'AuthMaster',
-        //     'email_verified_at' => Carbon::now(),
-        //     'created_at' => Carbon::now(),
-        //     'updated_at' => Carbon::now()
-        // ]);
-
-        // User::create([
-        //     'employee_id' => '',
-        //     'name'      => 'Abdul Majid',
-        //     'email'     => 'abdulmajid@nusa.net.id',
-        //     'password'  => bcrypt('12345678'),
-        //     'utype'     => 'AuthMaster',
-        //     'email_verified_at' => Carbon::now(),
-        //     'created_at' => Carbon::now(),
-        //     'updated_at' => Carbon::now()
-
-        // ]);
+        User::create([
+            'employee_id' => '0201926',
+            'name'      => 'Abdul Majid',
+            'email'     => 'abdulmajid@nusa.net.id',
+            'password'  => bcrypt('Nusanet2022!'),
+            'utype'     => 'AuthSales',
+            'isApprovedByAdmin'     => 1,
+            'email_verified_at' => Carbon::now(),
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now()
+        ]);
     }
 }
