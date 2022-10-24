e20c5b65c2349e713190029927a02753c4beea07
SamuelNusanetID <samuel@nusa.net.id>
2022-10-24 14:15:29 +0700

    Fix: Perbaikan CRUD Pengguna pada Admin Form

--- app/Http/Livewire/ManagerData/PenggunaComponent.php
+++ app/Http/Livewire/ManagerData/PenggunaComponent.php
@@ -16,13 +16,15 @@ class PenggunaComponent extends Component
 
     public $pengguna_search;
 
-    public $nama_pengguna_create,
+    public $id_pegawai_create,
+        $nama_pengguna_create,
         $email_pengguna_create,
         $password_pengguna_create,
         $konfirmasi_password_pengguna_create,
         $utype_pengguna_create;
 
-    public $nama_pengguna_set,
+    public $id_pegawai_set,
+        $nama_pengguna_set,
         $email_pengguna_set,
         $password_pengguna_set,
         $konfirmasi_password_pengguna_set,
@@ -50,17 +52,18 @@ class PenggunaComponent extends Component
     // Create User//
     public function create_pengguna()
     {
+        $this->validate([
+            'id_pegawai_create' => 'required|unique:users,employee_id',
+            'nama_pengguna_create' => 'required|unique:users,name',
+            'email_pengguna_create' => 'required|email|unique:users,email',
+            'password_pengguna_create' => 'min:6|required_with:konfirmasi_password_pengguna_create|same:konfirmasi_password_pengguna_create',
+            'konfirmasi_password_pengguna_create' => 'min:6',
+            'utype_pengguna_create' => 'required|string',
+        ]);
+
         try {
-            $this->validate([
-                'nama_pengguna_create' => 'required|unique:users,name',
-                'email_pengguna_create' => 'required|email|unique:users,email',
-                'password_pengguna_create' => 'min:6|required_with:konfirmasi_password_pengguna_create|same:konfirmasi_password_pengguna_create',
-                'konfirmasi_password_pengguna_create' => 'min:6',
-                'utype_pengguna_create' => 'required|string',
-
-            ]);
-
             $create_pengguna = new User();
+            $create_pengguna->employee_id = $this->id_pegawai_create;
             $create_pengguna->name = $this->nama_pengguna_create;
             $create_pengguna->email = $this->email_pengguna_create;
             $create_pengguna->password = Hash::make($this->password_pengguna_create);
@@ -84,11 +87,11 @@ class PenggunaComponent extends Component
                 'konfirmasi_password_pengguna_create',
                 'utype_pengguna_create',
             ]);
-        } catch (\Throwable) {
+        } catch (\Throwable $th) {
             $this->dispatchBrowserEvent('swal', [
                 'position' => 'centered',
                 'icon' => 'error',
-                'title' => 'Pengguna gagal tersimpan!',
+                'title' => $th->getMessage(),
                 'showConfirmButton' => false,
                 'timer' => 1500
             ]);
@@ -99,6 +102,7 @@ class PenggunaComponent extends Component
     public function pengguna_edit($id)
     {
         $pengguna_edit = User::find($id);
+        $this->id_pegawai_set = $pengguna_edit->employee_id;
         $this->nama_pengguna_set = $pengguna_edit->name;
         $this->email_pengguna_set = $pengguna_edit->email;
         $this->password_pengguna_set = $pengguna_edit->password;
@@ -112,15 +116,16 @@ class PenggunaComponent extends Component
     {
         try {
             $this->validate([
+                'id_pegawai_set' => 'required|string',
                 'nama_pengguna_set' => 'required|string',
                 'email_pengguna_set' => 'required|string',
                 'utype_pengguna_set' => 'required|string',
-                // 'password_pengguna_set' => 'required|string|min:8',
                 'konfirmasi_password_pengguna_set' => 'required|string'
 
             ]);
 
             $set_pengguna = User::where('id', $this->id_pengguna_set)->first();
+            $set_pengguna->employee_id = $this->id_pegawai_set;
             $set_pengguna->name = $this->nama_pengguna_set;
             $set_pengguna->email = $this->email_pengguna_set;
             $set_pengguna->password = $this->password_pengguna_set;
@@ -134,11 +139,11 @@ class PenggunaComponent extends Component
                 'showConfirmButton' => false,
                 'timer' => 1500
             ]);
-        } catch (\Throwable) {
+        } catch (\Throwable $th) {
             $this->dispatchBrowserEvent('swal', [
                 'position' => 'centered',
                 'icon' => 'error',
-                'title' => 'Edit Pengguna gagal tersimpan!',
+                'title' => $th->getMessage(),
                 'showConfirmButton' => false,
                 'timer' => 1500
             ]);
--- resources/views/livewire/manager-data/pengguna-component.blade.php
+++ resources/views/livewire/manager-data/pengguna-component.blade.php
@@ -40,6 +40,16 @@
                                 <form class="settings-form" wire:submit.prevent='create_pengguna'>
                                     @csrf
                                     <div class="mb-3">
+                                        <label for="setting-input-1" class="form-label">ID Pegawai :<span class="ms-2"
+                                                data-container="body" data-bs-toggle="popover" data-trigger="hover"
+                                                data-placement="top"
+                                                data-content="This is a Bootstrap popover example. You can use popover to provide extra info.">
+                                            </span>
+                                        </label>
+                                        <input type="text" class="form-control" id="setting-input-1"
+                                            wire:model='id_pegawai_create' required>
+                                    </div>
+                                    <div class="mb-3">
                                         <label for="setting-input-1" class="form-label">Nama :<span class="ms-2"
                                                 data-container="body" data-bs-toggle="popover" data-trigger="hover"
                                                 data-placement="top"
@@ -113,6 +123,16 @@
                                 <form class="settings-form" wire:submit.prevent='set_pengguna'>
                                     @csrf
                                     <div class="mb-3">
+                                        <label for="setting-input-1" class="form-label">ID Pegawai :<span
+                                                class="ms-2" data-container="body" data-bs-toggle="popover"
+                                                data-trigger="hover" data-placement="top"
+                                                data-content="This is a Bootstrap popover example. You can use popover to provide extra info.">
+                                            </span>
+                                        </label>
+                                        <input type="text" class="form-control" id="setting-input-1"
+                                            wire:model='id_pegawai_set' required>
+                                    </div>
+                                    <div class="mb-3">
                                         <label for="setting-input-1" class="form-label">Nama :<span class="ms-2"
                                                 data-container="body" data-bs-toggle="popover" data-trigger="hover"
                                                 data-placement="top"
@@ -131,22 +151,6 @@
                                         <input type="email" class="form-control" id="setting-input-1"
                                             wire:model='email_pengguna_set' required>
                                     </div>
-                                    {{-- <div class="mb-3">
-                                    <label for="setting-input-1" class="form-label">Password<span class="ms-2" data-container="body" data-bs-toggle="popover" data-trigger="hover" data-placement="top" data-content="This is a Bootstrap popover example. You can use popover to provide extra info."><svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-info-circle" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                                    <path fill-rule="evenodd" d="M8 15A7 7 0 1 0 8 1a7 7 0 0 0 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
-                                    <path d="M8.93 6.588l-2.29.287-.082.38.45.083c.294.07.352.176.288.469l-.738 3.468c-.194.897.105 1.319.808 1.319.545 0 1.178-.252 1.465-.598l.088-.416c-.2.176-.492.246-.686.246-.275 0-.375-.193-.304-.533L8.93 6.588z"/>
-                                    <circle cx="8" cy="4.5" r="1"/>
-                                    </svg></span></label>
-                                    <input type="password" class="form-control" id="setting-input-1" wire:model='password_pengguna_set' required>
-                                </div> --}}
-                                    {{-- <div class="mb-3">
-                                    <label for="setting-input-1" class="form-label">Konfirmasi Password<span class="ms-2" data-container="body" data-bs-toggle="popover" data-trigger="hover" data-placement="top" data-content="This is a Bootstrap popover example. You can use popover to provide extra info."><svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-info-circle" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                                    <path fill-rule="evenodd" d="M8 15A7 7 0 1 0 8 1a7 7 0 0 0 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
-                                    <path d="M8.93 6.588l-2.29.287-.082.38.45.083c.294.07.352.176.288.469l-.738 3.468c-.194.897.105 1.319.808 1.319.545 0 1.178-.252 1.465-.598l.088-.416c-.2.176-.492.246-.686.246-.275 0-.375-.193-.304-.533L8.93 6.588z"/>
-                                    <circle cx="8" cy="4.5" r="1"/>
-                                    </svg></span></label>
-                                    <input type="password" class="form-control" id="setting-input-1" wire:model='konfirmasi_password_pengguna_set' required>
-                                </div> --}}
                                     <div class="mb-3">
                                         <label for="setting-input-2" class="form-label">Daftar Sebagai</label>
                                         <select class="form-control" id="setting-input-2"
@@ -250,22 +254,6 @@
                                         <input type="email"readonly class="form-control" id="setting-input-1"
                                             wire:model='email_pengguna_delete' required>
                                     </div>
-                                    {{-- <div class="mb-3">
-                                    <label for="setting-input-1" class="form-label">Password<span class="ms-2" data-container="body" data-bs-toggle="popover" data-trigger="hover" data-placement="top" data-content="This is a Bootstrap popover example. You can use popover to provide extra info."><svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-info-circle" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                                    <path fill-rule="evenodd" d="M8 15A7 7 0 1 0 8 1a7 7 0 0 0 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
-                                    <path d="M8.93 6.588l-2.29.287-.082.38.45.083c.294.07.352.176.288.469l-.738 3.468c-.194.897.105 1.319.808 1.319.545 0 1.178-.252 1.465-.598l.088-.416c-.2.176-.492.246-.686.246-.275 0-.375-.193-.304-.533L8.93 6.588z"/>
-                                    <circle cx="8" cy="4.5" r="1"/>
-                                    </svg></span></label>
-                                    <input type="password"readonly class="form-control" id="setting-input-1" wire:model='password_pengguna_delete' required>
-                                </div>
-                                <div class="mb-3">
-                                    <label for="setting-input-1" class="form-label">Konfirmasi Password<span class="ms-2" data-container="body" data-bs-toggle="popover" data-trigger="hover" data-placement="top" data-content="This is a Bootstrap popover example. You can use popover to provide extra info."><svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-info-circle" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                                    <path fill-rule="evenodd" d="M8 15A7 7 0 1 0 8 1a7 7 0 0 0 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
-                                    <path d="M8.93 6.588l-2.29.287-.082.38.45.083c.294.07.352.176.288.469l-.738 3.468c-.194.897.105 1.319.808 1.319.545 0 1.178-.252 1.465-.598l.088-.416c-.2.176-.492.246-.686.246-.275 0-.375-.193-.304-.533L8.93 6.588z"/>
-                                    <circle cx="8" cy="4.5" r="1"/>
-                                    </svg></span></label>
-                                    <input type="password"readonly class="form-control" id="setting-input-1" wire:model='konfirmasi_password_pengguna_delete' required>
-                                </div> --}}
                                     <div class="mb-3">
                                         <label for="setting-input-2"readonly class="form-label">Daftar Sebagai</label>
                                         <select class="form-control"readonly id="setting-input-2"
